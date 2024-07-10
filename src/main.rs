503defc1fac4ccba0c399373f3b9817109f42b49
Lorenz Reithmayr <lorenz.reithmayr@posteo.de>
2024-07-10 18:18:25 +0200

    Parsing equal signs

--- src/main.rs
+++ src/main.rs
@@ -105,24 +105,29 @@ impl Token {
     }
 }
 
-struct Lexer {
-}
+struct Lexer {}
 
 impl Lexer {
     fn lex(file: &str) -> i32 {
         let mut error_code: i32 = 0;
         let mut line_nr: i32 = 1;
-        let mut skip: bool = false;
         let mut tokens = Vec::new();
 
         let mut f_iter = file.chars().peekable();
         while let Some(c) = f_iter.next() {
             let next_char = f_iter.peek();
-            let t = Self::scan_char(c, next_char, &mut line_nr, &mut skip);
-            tokens.push(t);
+            if let Some(t) = Self::scan_char(c, &mut line_nr) {
+                match t.token_type {
+                    TokenType::EQUAL if next_char == Some(&'=') => {
+                        tokens.push(Token::new( TokenType::EQUAL_EQUAL, String::from("=="), line_nr, ));
+                        f_iter.nth(0);
+                    }
+                    _ => {tokens.push(t)}
+                }
+            }
         }
 
-        for t in tokens.into_iter().flatten() {
+        for t in tokens.into_iter() {
             if t.token_type == TokenType::LEXICAL_ERROR {
                 eprintln!(
                     "[line {}] Error: Unexpected character: {}",
@@ -137,11 +142,7 @@ impl Lexer {
         error_code
     }
 
-    fn scan_char(c: char, n: Option<&char>, line_nr: &mut i32, skip: &mut bool) -> Option<Token> {
-        if *skip {
-            *skip = false;
-            return None;
-        }
+    fn scan_char(c: char, line_nr: &mut i32) -> Option<Token> {
         match c {
             '(' => Some(Token::new(TokenType::LEFT_PAREN, String::from(c), *line_nr)),
             ')' => Some(Token::new(
@@ -165,29 +166,13 @@ impl Lexer {
                 *line_nr += 1;
                 None
             }
-            '=' => {
-                if let Some(n) = n {
-                    if Self::match_next(*n, '=') {
-                        *skip = true;
-                        return Some(Token::new(
-                            TokenType::EQUAL_EQUAL,
-                            String::from("=="),
-                            *line_nr,
-                        ));
-                    }
-                }
-                Some(Token::new(TokenType::EQUAL, String::from(c), *line_nr))
-            }
+            '=' => Some(Token::new(TokenType::EQUAL, String::from(c), *line_nr)),
             _ => Some(Token::new(
                 TokenType::LEXICAL_ERROR,
                 String::from(c),
                 *line_nr,
             )),
         }
-    }
-
-    fn match_next(next: char, comp: char) -> bool {
-        next == comp
     }
 }
 
